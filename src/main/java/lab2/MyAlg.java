e6eda4711a8fc20d0db000b704157124c2ec5795
laurenplf <pierre-louis.laurent@depinfonancy.net>
2020-01-22 01:58:00 +0100

    2 types of crossover and a simplified mutation adapted to changes of dimension

--- src/main/java/lab2/MyAlg.java
+++ src/main/java/lab2/MyAlg.java
@@ -12,9 +12,9 @@ import java.util.Random;
 public class MyAlg {
 
     public static void main(String[] args) {
-        int dimension = 2; // dimension of problem
+        int dimension = 100; // dimension of problem
         int populationSize = 10; // size of population
-        int generations = 10; // number of generations
+        int generations = 100000; // number of generations
 
         Random random = new Random(); // random
 
--- src/main/java/lab2/MyCrossover.java
+++ src/main/java/lab2/MyCrossover.java
@@ -15,12 +15,45 @@ public class MyCrossover extends AbstractCrossover<double[]> {
         ArrayList children = new ArrayList();
 
         // your implementation:
-        double[] child1 = new double[2];
-        double[] child2 = new double[2];
-        child1[0] = p1[0];
-        child1[1] = p2[1];
-        child2[0] = p2[0];
-        child2[1] = p1[1];
+        double[] child1 = new double[p1.length];
+        double[] child2 = new double[p1.length];
+        if (random.nextDouble() < 0.5) {
+            if (random.nextDouble() < 0.5) {
+                for (int j = 0; j < (p1.length - 1) / 2; j++) {
+                    child1[2 * j] = p1[2 * j];
+                    child2[2 * j] = p2[2 * j];
+                }
+                for (int j = 1; j < p1.length / 2; j++) {
+                    child1[2 * j - 1] = p2[2 * j - 1];
+                    child2[2 * j - 1] = p1[2 * j - 1];
+                }
+            } else {
+                for (int j = 0; j < (p1.length - 1) / 2; j++) {
+                    child2[2 * j] = p1[2 * j];
+                    child1[2 * j] = p2[2 * j];
+                }
+                for (int j = 1; j < p1.length / 2; j++) {
+                    child2[2 * j - 1] = p2[2 * j - 1];
+                    child1[2 * j - 1] = p1[2 * j - 1];
+                }
+            }
+        } else {
+            int remaining = p1.length/2;
+            int index = i;
+            for (int j = 0 ; j < p1.length ; j++) {
+                if (remaining > 0) {
+                    child1[index] = p1[index];
+                    child2[index] = p2[index];
+                } else {
+                    child1[index] = p2[index];
+                    child2[index] = p1[index];
+                }
+
+                index++;
+                index = index%p1.length;
+                remaining--;
+            }
+        }
         children.add(child1);
         children.add(child2);
         return children;
